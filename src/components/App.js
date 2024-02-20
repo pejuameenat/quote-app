22489eeeb8f482fc96ff94498915ce7ac0597739
pejuameenat <104262132+pejuameenat@users.noreply.github.com>
2024-02-20 23:46:23 +0100

    state changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./Card";
 import quotes from "../quotes";
-
-function generatQote() {
-  const heading = document.querySelector("h2");
-  const paragraph = document.querySelector("p");
-  const cards = document.querySelector(".card");
-  const randomNum = Math.floor(Math.random() * quotes.length);
-  heading.textContent = quotes[randomNum].word;
-  paragraph.textContent = quotes[randomNum].author;
-  cards.style.backgroundImage = quotes[randomNum].bgImage;
-}
+ 
 function App() {
+  const [randomNum, setrandomNum] = useState(0)
+  const [quote, setQUote] =useState(quotes)
+  function generatQote() {
+  setrandomNum(Math.floor(Math.random() * quotes.length));
+}
   return (
     <div className="container">
       <div className="innner-wrapper">
         <h1>Quote Generating App</h1>
-        <Card
-          words="What we achieve inwardly will change outer reality"
-          author="Plutarch"
-          heading="Quote of the day"
+        <Card 
+          words={quote[randomNum].word}
+          author={quote[randomNum].author}
+          heading='Daily Quotes'
+          quotes={quote}
+          randomNum={randomNum}
         />
         <button className="generate" onClick={generatQote}>
           Generate Quote
         </button>
       </div>
     </div>
-  );
+  )
 }
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 function Card(props) {
+  const styles = {
+    backgroundImage: props.quotes[props.randomNum].bgImage
+  }
   return (
-    <div className="card">
+    <div className="card" style={styles}>
       <h4>{props.heading}</h4>
       <h2>{props.words}</h2>
       <p>{props.author}</p>
